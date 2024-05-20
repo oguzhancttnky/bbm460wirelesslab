d167cbbbf0ee2724b6e4a59286afaac2107191cc
oguzhancttnky <oguzhancttnky@gmail.com>
2024-05-20 18:02:09 +0300

    fixed issues

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -20,15 +20,34 @@ function App() {
     return () => clearInterval(intervalId);
   }, []);
 
+  const getStatusColor = (status) => {
+    switch (status) {
+      case 'High Risk!':
+        return '#FF4433';
+      case 'Moderate Risk':
+        return '#FFAA33';
+      case 'Low Risk':
+        return '#FCF55F';
+      case 'Safe':
+        return '#228B22';
+      default:
+        return '#000';
+    }
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Temperature & Humidity Dashboard</h1>
-        <h3>Fire Rate Status: {data.status}</h3>
-        <span class="thermometer">{data.temperature.toFixed(2)}&deg;C</span>
-        <p>Temperature: {data.temperature.toFixed(2)} °C</p>
-        <p>Humidity: {data.humidity.toFixed(2)}%</p>
+    <div className="app">
+      <header className="app-header">
+        <h1 className="title">Wireless Over Temperature Alarm Project Dashboard</h1>
       </header>
+      <main className="app-content">
+        <div className="card">
+          <h3 className="status">Fire Rate Status: <span style={{ color: getStatusColor(data.status) }}>{data.status}</span></h3>
+          <span class="thermometer">{data.temperature.toFixed(2)}&deg;C</span>
+          <p className="data">Temperature: <span style={{ color: getStatusColor(data.status) }}>{data.temperature.toFixed(2)} &deg;C</span></p>
+          <p className="data">Humidity: <span style={{ color: getStatusColor(data.status) }}>{data.humidity.toFixed(2)}%</span></p>
+        </div>
+      </main>
     </div>
   );
 }
